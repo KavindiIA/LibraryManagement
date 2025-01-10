341872bf1f1e84cef6b283d349397c780b61613b
KavindiIA <kavindiimasha2004@gmail.com>
2025-01-10 16:18:55 +0530

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module LibraryManagement {
+	requires java.desktop;
+	requires java.sql;
+	requires jcalendar;
+	requires jBCrypt;
+	requires java.mail;
+	requires net.sf.jasperreports.core;
+    requires org.apache.commons.logging; 
+    requires net.sf.jasperreports.fonts; 
+    requires net.sf.jasperreports.spring;
+}
